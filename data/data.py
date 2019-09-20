107a2be97d7e74549482acf095036a8b9cd42b54
vaisanem <miikkamv@gmail.com>
2019-09-20 18:10:13 +0300

    read test data in ADDED function test_data that returns test data as strings in two dimensional numpy array MODIFIED attributes function to include top-level attributes

--- data/data.py
+++ data/data.py
@@ -1,13 +1,20 @@
 import xlrd
+import numpy as np
+
+data = xlrd.open_workbook("data/luokat_kaikki.xls")
 
 def attributes():
-  data = xlrd.open_workbook("luokat_kaikki.xls")
   keys = data.sheet_by_name("avain")
-  attributes = []
+  attributes = {}
   for row in keys.get_rows():
-    if row[0].ctype == 1:
-      attributes.append({
-        "attribute_fi": row[1].value.encode("utf-8"),
-        "attribute_id": int(row[0].value)
-      })
+    if row[0].ctype != 0:
+      attributes[str(row[0].value)] = row[1].value.encode("utf-8")
   return attributes
+
+def test_data():
+  test = data.sheet_by_name("testi")
+  classes = []
+  for row in test.get_rows():
+    classes.append(np.array(map(lambda one: one.value, row)))
+  return np.array(classes).astype(str)
+
