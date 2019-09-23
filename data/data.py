210b572a84a543eb8b86616b7ccefd66c2c9ce2e
vaisanem <miikkamv@gmail.com>
2019-09-23 13:49:44 +0300

    imported data.py in app.py ADDED initial way of choosing next question data/__init__.py descriptive comments in data.py

--- data/data.py
+++ data/data.py
@@ -3,15 +3,18 @@ import numpy as np
 
 data = xlrd.open_workbook("data/luokat_kaikki.xls")
 
+#returns dictionary made up from attribute code and name pairs
 def attributes():
   keys = data.sheet_by_name("avain")
   attributes = {}
   for row in keys.get_rows():
+    #ignore empty cells
     if row[0].ctype != 0:
       attributes[float(row[0].value)] = row[1].value.encode("utf-8")
   return attributes
 
-def test_data():
+#returns two-dimensional numpy array of building class data
+def building_classes():
   test = data.sheet_by_name("testi")
   classes = []
   for row in test.get_rows():
