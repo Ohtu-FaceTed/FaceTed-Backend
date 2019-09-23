b38f8aeb99d138bd0a8c60c0092adace8e8b151a
alafuzof <aleksander.alafuzoff@gmail.com>
2019-09-23 08:27:54 +0300

    Return id fields as strings to keep leading zeroes MODIFIED: > app.py

--- app.py
+++ app.py
@@ -21,14 +21,14 @@ def answer():
                 'message': 'Please supply "language", "attribute_id", and "response" in query'}
     else:
         return {'success': True,
-                'new_question': {'attribute_id': 101, 'attribute_name': 'Asuinhuone'},
-                'building_classes': [{'class_id': 110, 'class_name': 'Omakotitalot', 'score': 0.9},
-                                     {'class_id': 320, 'class_name': 'Hotellit', 'score': 0.5},
-                                     {'class_id': 1311, 'class_name': 'Väestönsuojat', 'score': 0.1}]}
+                'new_question': {'attribute_id': '0101', 'attribute_name': 'Asuinhuone'},
+                'building_classes': [{'class_id': '0110', 'class_name': 'Omakotitalot', 'score': 0.9},
+                                     {'class_id': '0320', 'class_name': 'Hotellit', 'score': 0.5},
+                                     {'class_id': '1311', 'class_name': 'Väestönsuojat', 'score': 0.1}]}
 
 @app.route('/question', methods=['GET'])
 def question():
-    return {'attribute_id': 101, 'attribute_name': 'Asuinhuone'}
+    return {'attribute_id': '0101', 'attribute_name': 'Asuinhuone'}
 
 if __name__ == "__main__":
     # Parse command line arguments
