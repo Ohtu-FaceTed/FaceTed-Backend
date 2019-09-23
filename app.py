fff6dd95ec62a6ddff7c2953e8c57cb32785c61a
alafuzof <aleksander.alafuzoff@gmail.com>
2019-09-23 09:13:43 +0300

    Added server port command line option MODIFIED > app.py

--- app.py
+++ app.py
@@ -34,6 +34,7 @@ if __name__ == "__main__":
     # Parse command line arguments
     parser = argparse.ArgumentParser(description='Run Faceted-Search Flask backend')
     parser.add_argument('--debug', action='store_true', help='start Flask in debug mode (DANGEROUS!)')
+    parser.add_argument('--port', default=5000, help='server port')
     args = parser.parse_args()
 
-    app.run(debug=args.debug)
+    app.run(debug=args.debug, port=args.port)
