eb87524e482221e7d7cb0a9912278a05eb19af2a
alafuzof <aleksander.alafuzoff@gmail.com>
2019-10-12 15:47:48 +0300

    Added support for --profile switch in app, which enables the Werkzeug profiler MODIFIED > app.py

--- app.py
+++ app.py
@@ -12,10 +12,19 @@ if __name__ == "__main__":
     parser.add_argument('--port', default=5000, help='server port')
     parser.add_argument('--host', default='0.0.0.0', help='server host')
     parser.add_argument('--data_directory', default='./data', help='data directory')
+    parser.add_argument('--profile', action='store_true', 
+                        help='start Flask with profiler enabled (implies --debug, DANGEROUS!)')
     args = parser.parse_args()
 
     # Load data from the supplied directorytand replace the default objects
     src.building_data = src.BuildingData(args.data_directory)
     src.classifier = src.NaiveBayesClassifier(src.building_data.observations)
 
-    app.run(debug=args.debug, port=args.port, host=args.host)
+    if not args.profile:
+        app.run(debug=args.debug, port=args.port, host=args.host)
+    else:
+        from werkzeug.middleware.profiler import ProfilerMiddleware
+        app.config['PROFILE'] = True
+        # Config profiler to show 30 most expensive functions
+        app.wsgi_app = ProfilerMiddleware(app.wsgi_app, restrictions=[30])
+        app.run(debug=True, port=args.port, host=args.host)
