7d056d42e513dcd9c0cf6a34934b4a0df92f1d75
Mikko Lipsanen <mikko.lipsanen@helsinki.fi>
2019-10-13 15:27:35 +0300

    Added test for user data creation

--- tests/test_api.py
+++ tests/test_api.py
@@ -122,6 +122,14 @@ def test_session_gets_recreated_for_client_requesting_first_question(backend):
         assert previous_id != ''
         assert session['user'] != previous_id
 
+def test_if_user_in_session_user_data_is_created_after_first_question(backend):
+    with backend:
+        backend.get('/question')
+        users.pop(session['user'], None)
+        assert session['user'] not in users
+        backend.post('/answer', json={'attribute_id': '1', 'response': 'yes'})
+        assert session['user'] in users
+
 def test_same_questions_not_repeated_during_session(responses):
     questions = responses['questions']
     attributes = responses['attributes']
