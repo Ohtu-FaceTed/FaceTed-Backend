c25658d6a4f7e5c599b3b693ef506b808701a692
alafuzof <aleksander.alafuzoff@gmail.com>
2019-10-23 09:54:52 +0300

    Added BuildingClass and Attribute SQL models, Changed SQL models to match modified schema. Moved database init to models. Models are now derived from db.Model. ADDED > src/models/answer.py > src/models/attribute.py > src/models/building_class.py REMOVED > src/models/question.py MODIFIED > src/models/__init__.py

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,3 +1,21 @@
-from sqlalchemy.ext.declarative import declarative_base
+from .. import app
 
-Base = declarative_base()
+# SQLAlchemy import and setup
+from flask_sqlalchemy import SQLAlchemy
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite://app.db"
+# prints for debugging
+app.config["SQLALCHEMY_ECHO"] = True
+
+db = SQLAlchemy(app)
+
+
+# create tables
+try:
+    db.create_all()
+except:
+    pass
+
+
+#from sqlalchemy.ext.declarative import declarative_base
+
+#Base = declarative_base()
