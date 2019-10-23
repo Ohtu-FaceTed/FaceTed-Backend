d291dfe03f2865c74c85611f5f319b27cf9a2309
alafuzof <aleksander.alafuzoff@gmail.com>
2019-10-23 09:54:52 +0300

    Added BuildingClass and Attribute SQL models, Changed SQL models to match modified schema. Moved database init to models. Models are now derived from db.Model. ADDED > src/models/answer.py > src/models/attribute.py > src/models/building_class.py REMOVED > src/models/question.py MODIFIED > src/models/__init__.py

---
+++ src/models/attribute.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, String
+from . import db
+
+class Attribute(db.Model):
+    __tablename__ = 'attribute'
+
+    id = Column(Integer, primary_key=True)
+    attribute_id = Column(String(12), nullable=False)
+    attribute_name = Column(String(64), nullable=False)
+    attribute_question = Column(String(64), nullable= False)
+
+    def __repr__(self):
+        return f"<Attribute(attribute_id='{self.attribute_id}', attribute_name='{self.attribute_name}', attribute_question='{self.attribute_question}')>"
