060dbea96c37f9ad985f6a41b26d9420d5b5cb06
EssKayz <esskay@protonmail.com>
2019-10-24 20:55:30 +0300

    Lintting
    Forgot to lint, now linting.

--- src/models/answer_question.py
+++ src/models/answer_question.py
@@ -11,16 +11,16 @@ class AnswerQuestion(db.Model):
     attribute_id = Column(Integer, ForeignKey('attribute.id'))
     answer_id = Column(Integer, ForeignKey('answer.id'))
     session_id = Column(Integer, ForeignKey('session.id'))
-    
+
     attribute = relationship("attribute")
     answer = relationship("answer")
-    
+
     session = relationship("session", back_populates="answer_question")
 
-    def __init__(self,question, answer, session):
+    def __init__(self, question, answer, session):
         self.question_id = question.id
         self.answer_id = answer.id
         self.session_id = answer.id
-        
+
     def __repr__(self):
         return f"<AnswerQuestion(question_id='{self.question_id}', answer_id='{self.answer_id}')>"
