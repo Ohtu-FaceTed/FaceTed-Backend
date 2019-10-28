ec899282c58655342ef6d00e5e2607aaf72de76c
alafuzof <aleksander.alafuzoff@gmail.com>
2019-10-28 09:08:59 +0200

    Moved database initialization to separate module (cf. views) MODIFIED > src/__init__.py > src/models/__init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -2,27 +2,21 @@ import os
 from flask import Flask, escape, request
 app = Flask(__name__)
 
-# SQLAlchemy import and setup
-from flask_sqlalchemy import SQLAlchemy
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite://app.db"
 # prints for debugging
 app.config["SQLALCHEMY_ECHO"] = True
-
-db = SQLAlchemy(app)
-
-
-from flask_cors import CORS
-#from src import answer
-#from src import question
-#from src import previous
-
 app.config["SECRET_KEY"] = os.urandom(32)
 # load actual secret key
 app.config.from_pyfile('../config.py')
+
+from flask_cors import CORS
 CORS(app, supports_credentials=True)
 
 from . import views
 views.init_app(app)
+
+from . import models
+models.init_app(app)
 
 from .naive_bayes_classifier import NaiveBayesClassifier
 from .building_data import BuildingData
