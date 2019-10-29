ff8ac4a48f994b4926c82700d8fb64550be85894
alafuzof <aleksander.alafuzoff@gmail.com>
2019-10-30 01:33:05 +0200

    Removed unused function in question selection MODIFIED > src/question_selection.py

--- src/question_selection.py
+++ src/question_selection.py
@@ -2,53 +2,6 @@ import numpy as np
 import src
 
 
-def entropy(probabilities):
-    '''Calculates entropy with given probabilities P(Y|X)'''
-    H = 0
-    for i in probabilities['posterior']:
-        H += i * np.log(i)
-    H = H * (-1)
-
-    return H
-
-
-def new_entropy(attribute):
-    '''Calculates new entropy for question considering both answers'''
-    from flask import session
-    from src.sessionManagement import users
-    # selects the previous probabilities as prior for calculating posterior
-    prior = None
-    if users[session['user']]['probabilities']:
-        prior = users[session['user']]['probabilities'][-1]
-
-    probabilites_yes = src.classifier.calculate_posterior(
-        attribute, 'yes', prior)
-    N0 = np.sum(probabilites_yes['posterior'])  # Maybe this?
-    probabilites_no = src.classifier.calculate_posterior(
-        attribute, 'no', prior)
-    N1 = np.sum(probabilites_no['posterior'])  # Maybe this?
-
-    return entropy(probabilites_yes) * N0 + entropy(probabilites_no) * N1
-
-
-def best_questions_old():
-    '''Sorts remaining questions from best to worst based on their entropies'''
-    from flask import session
-    from src.sessionManagement import users
-    # Questions ordered from lowest to highest entropy
-    entropies = []
-    for i in src.building_data.attribute_name.keys():
-        # Skip already asked questions
-        if i in users[session['user']]['attributes']:
-            continue
-        H = new_entropy(i)
-        entropies.append((i, H))
-
-    entropies = sorted(entropies, key=lambda x: x[1])
-
-    return entropies
-
-
 def best_questions(prior, answered_questions):
     '''Returns attribute with lowest resultant entropy of posteriors'''
 
