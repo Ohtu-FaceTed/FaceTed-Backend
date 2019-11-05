829f5c9dfe8192fe7aa744203b09814a094448d6
alafuzof <aleksander.alafuzoff@gmail.com>
2019-11-05 23:45:27 +0200

    Fixed loading of database models. MODIFIED > src/models/__init__.py > src/models/answer.py

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -7,6 +7,13 @@ db = SQLAlchemy()
 def init_app(app):
     db.init_app(app)
 
+    from .answer import Answer
+    from .answer_question import AnswerQuestion
+    from .attribute import Attribute
+    from .building_class import BuildingClass
+    from .session import Session
+
+
     with app.app_context():
         try:
             db.create_all()
