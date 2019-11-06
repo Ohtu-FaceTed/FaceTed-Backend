29e9b7a1a5f47aa275e30c05ddaa8ffb3f620ede
alafuzof <aleksander.alafuzoff@gmail.com>
2019-11-06 07:27:06 +0200

    Added SQL saving of user sessions MODIFIED > src/views/question.py

--- src/views/question.py
+++ src/views/question.py
@@ -2,6 +2,7 @@ from src.sessionManagement import generate_id, users
 from src.question_selection import next_question
 from flask import jsonify, session
 from . import views as app
+from ..models import db, Session
 
 
 @app.route('/question', methods=['GET'])
@@ -13,7 +14,11 @@ def question():
     ident = generate_id()
     session['user'] = ident
     users[ident] = {'probabilities': [], 'answers': [],
-                    'questions': [], 'question_strings': [], 'attributes': []}
+                    'questions': [], 'question_strings': [], 'attributes': [],
+                    'db_session': Session(ident)}
+    # Add the session to the database
+    db.session.add(users[ident]['db_session'])
+    db.session.commit()
     question = next_question(None, [])
     users[ident]['questions'].append(question['attribute_name'])
     users[ident]['question_strings'].append(question['attribute_question'])
