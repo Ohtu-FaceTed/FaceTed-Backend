0129880cfb76d684bf10e2cf30151527c407c68b
Kim Jämsen <jamsen.kim@gmail.com>
2019-11-13 21:44:55 +0200

    Update __init__.py
    MODIFIED
    >test/__init__.py
    - test data now corresponds to the way it is in the db.

--- tests/__init__.py
+++ tests/__init__.py
@@ -12,36 +12,36 @@ def init_test_db(app):
         # Initialize question_group table
         db.session.add(QuestionGroup(grouping_key='1',
                                      group_name='WC:t',
-                                     group_question='Minkälaisia WC-tiloja rakennuksessa on?'))
+                                     group_question="{'fi':'Minkälaisia WC-tiloja rakennuksessa on?', 'sv':'[Svenska]Hurudana toaletter innehåller byggnaden?', 'en':'[English]What kinds of restrooms does the space contain?'}"))
 
         # Initialize attribute table
         db.session.add(Attribute(attribute_id='1',
-                                 attribute_name='Asunnot',
-                                 attribute_question='Onko rakennuksessa asunnot?',
+                                 attribute_name="{'fi':'Asunnot', 'sv':'[Svenska]Asunnot', 'en':'[English]Asunnot'}",
+                                 attribute_question="{'fi':'Onko rakennuksessa asunnot?', 'sv':'[Svenska]Onko rakennuksessa asunnot?', 'en':'[English]Onko rakennuksessa asunnot?'}",
                                  grouping_id=None,
                                  active=True,
                                  attribute_tooltip=''))
         db.session.add(Attribute(attribute_id='101',
-                                 attribute_name='Asuinhuone',
-                                 attribute_question='Onko rakennuksessa asuinhuone?',
+                                 attribute_name="{'fi':'Asuinhuone', 'sv':'[Svenska]Asuinhuone', 'en':'[English]Asuinhuone'}",
+                                 attribute_question="{'fi':'Onko rakennuksessa asuinhuone?', 'sv':'[Svenska]Onko rakennuksessa asuinhuone?', 'en':'[English]Onko rakennuksessa asuinhuone?'}",
                                  grouping_id=None,
                                  active=True,
                                  attribute_tooltip=''))
         db.session.add(Attribute(attribute_id='102',
-                                 attribute_name='Eteinen',
-                                 attribute_question='Onko rakennuksessa eteinen?',
+                                 attribute_name="{'fi':'Eteinen', 'sv':'[Svenska]Eteinen', 'en':'[English]Eteinen'}",
+                                 attribute_question="{'fi':'Onko rakennuksessa eteinen?', 'sv':'[Svenska]Onko rakennuksessa eteinen?', 'en':'[English]Onko rakennuksessa eteinen?'}",
                                  grouping_id=None,
                                  active=True,
                                  attribute_tooltip=''))
         db.session.add(Attribute(attribute_id='114',
-                                 attribute_name='WC',
-                                 attribute_question='Onko rakennuksessa WC?',
+                                 attribute_name="{'fi':'WC', 'sv':'[Svenska]WC', 'en':'[English]WC'}",
+                                 attribute_question="{'fi':'Onko rakennuksessa wc?', 'sv':'[Svenska]Onko rakennuksessa wc?', 'en':'[English]Onko rakennuksessa wc?'}",
                                  grouping_id='1',
                                  active=True,
                                  attribute_tooltip=''))
         db.session.add(Attribute(attribute_id='116',
-                                 attribute_name='WC-pesuhuone',
-                                 attribute_question='Onko rakennuksessa WC-pesuhuone?',
+                                 attribute_name="{'fi':'WC-pesuhuone', 'sv':'[Svenska]WC-pesuhuone', 'en':'[English]WC-pesuhuone'}",
+                                 attribute_question="{'fi':'Onko rakennuksessa wc-pesuhuone?', 'sv':'[Svenska]Onko rakennuksessa wc-pesuhuone?', 'en':'[English]Onko rakennuksessa wc-pesuhuone?'}",
                                  grouping_id='1',
                                  active=True,
                                  attribute_tooltip=''))
