a15e37286e8a4104655f60ba561f485043641fb0
EssKayz <esskay@protonmail.com>
2019-11-13 21:53:00 +0200

    Update __init__.py
    MODIFIED:
    > New method for getting best matching language out of supported languages
    > new method for parsing correct language string from json

--- src/views/__init__.py
+++ src/views/__init__.py
@@ -1,7 +1,10 @@
+import json
 from flask import Blueprint
 
 views = Blueprint('views', __name__)
 
+default_language = 'fi'
+supported_languages = ['en', 'fi', 'se']
 
 def init_app(app):
     with app.app_context():
@@ -16,3 +19,19 @@ def init_app(app):
 
         # Register blueprint
         app.register_blueprint(views)
+
+def get_best_match_language(languages):
+        for lang in languages.values():
+            if lang in supported_languages:
+                return lang
+
+def select_question_by_language(questionJson, language):
+    question = json.loads(questionJson)
+
+    if language in supported_languages:
+        if language is not None:
+            return question[language]
+        else:
+            return question[default_language]
+    else:
+        print('language ' + lang + ' not supported for attribute : ' + questionJson)
