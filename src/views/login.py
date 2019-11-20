00e4faedd422c450d3429c675697fc47aec0b5f9
xTooth <jamsen.kim@gmail.com>
2019-11-20 12:00:39 +0200

    Update login.py
    LOGIN now works BUT is insanely persistent.
    TODO: bcrypt.

--- src/views/login.py
+++ src/views/login.py
@@ -7,29 +7,30 @@ from flask_login import login_required, login_user, logout_user, current_user
 @app.route("/801fc31", methods = ["GET", "POST"])
 def auth_login():
     if request.method == "GET":
+        print("login was called")
         return render_template("admLogin.html")
 
     form = request.form
-    pw = form['password']
-    username = form['username']
+    pw = form.get('password')
+    username = form.get('username')
     user = Admin.query.filter_by(username = username).first()
     
     if not user:
          return render_template("admLogin.html", form = form,
                                error = "No such username or password")
 
-    if not bcrypt.check_password_hash(user.password, pw):
+    if pw != user.password :
         return render_template("admLogin.html", form = form,
                                error = "No such username or password")
 
 
     print("User " + user.name + " was identified")
     login_user(user)
-    return redirect(url_for("index"))
+    return redirect(url_for("views.admin_view"))
 
 #logout handler
 @app.route("/auth/logout")
 @login_required
 def auth_logout():
     logout_user()
-    return redirect(url_for("index"))
+    return redirect(url_for("views.auth_login"))
