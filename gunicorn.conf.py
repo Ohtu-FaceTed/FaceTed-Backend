bcaad3dea389ab6ab27125d6722a94e70a03eaab
Tommi Koivula <tommi.koivula93@gmail.com>
2019-12-01 13:22:10 +0200

    modify gunicorn conf

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -5,16 +5,14 @@ import os
 # Address where the app will be served at
 bind = "127.0.0.1:5000"
 # Change process name
-name = "faceted"
 
 workers = 1
-check_config = True
 
 # Enable logging at dir logs and pip output there
 DIR = os.path.dirname(os.path.abspath(__file__))
 access_logfile = os.path.join(DIR, "logs/access.log")
 error_logfile = os.path.join(DIR, "logs/error.log")
-capture_output = True
+capture_output = False
 # Detach from the console and run in background
 daemon = False
 
