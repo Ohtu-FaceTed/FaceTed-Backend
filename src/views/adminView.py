8aba0cc89a4bbddefb415e675e8c08e358ddaa47
Mikko Lipsanen <mikko.lipsanen@helsinki.fi>
2019-12-03 18:55:57 +0200

    Possibility to change group id for attributes in admin view

--- src/views/adminView.py
+++ src/views/adminView.py
@@ -1,6 +1,6 @@
 import json
 from . import views as app
-from ..models import db, Answer, AnswerQuestion, Attribute, Session
+from ..models import db, Answer, AnswerQuestion, Attribute, Session, QuestionGroup
 from flask import redirect, render_template, request, url_for, jsonify
 from flask_login import login_required
 
@@ -8,7 +8,7 @@ from flask_login import login_required
 @app.route("/801fc3", methods=["GET"])
 @login_required
 def admin_view():
-    return render_template("admView.html", attributes=Attribute.query.all())
+    return render_template("admView.html", attributes=Attribute.query.all(), groups = QuestionGroup.query.all())
 
 
 # question string edit
@@ -143,4 +143,16 @@ def setActive(attribute_id):
         attr.active = True
 
     db.session.commit()
+    return redirect(url_for("views.admin_view"))
+
+
+@app.route("/gs95b/<attribute_id>", methods=["POST"])
+@login_required
+def setGroup(attribute_id):
+    attr = Attribute.query.get(attribute_id)
+    selected_group = request.form.get('select_group')
+    if attr.grouping_id != selected_group: 
+        attr.grouping_id == selected_group
+        db.session.commit()
+
     return redirect(url_for("views.admin_view"))
